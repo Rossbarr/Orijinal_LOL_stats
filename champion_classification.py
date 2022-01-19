81d619ed08c665e9c513d6f1cbdf51e414515b81
rossbarr <barrettross23@gmail.com>
2022-01-18 19:25:55 -0500

    Check accuracy of ML model

--- champion_classification.py
+++ champion_classification.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 from sklearn import svm
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_curve, accuracy_score
 
 champs = cg.champion_get()
 
@@ -128,4 +129,6 @@ data_train, data_test, tags_train, tags_test = train_test_split(data, tags.value
 clf = svm.SVC()
 clf.fit(data_train, tags_train)
 
-clf.predict(data_test)
+tags_pred = clf.predict(data_test)
+
+score = accuracy_score(tags_test, tags_pred)
