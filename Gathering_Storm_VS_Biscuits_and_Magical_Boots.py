6f5b400cc73977462f587e41dd116ae6befbded6
rossbarr <barrettross23@gmail.com>
2022-01-19 10:16:07 -0500

    Small edits for readability

--- Gathering_Storm_VS_Biscuits_and_Magical_Boots.py
+++ Gathering_Storm_VS_Biscuits_and_Magical_Boots.py
@@ -27,10 +27,9 @@ def magical_footwear_gold_value(time):
     for i in time:
         if i >= 12:
             x[q] = move_speed_gold_value(25+10)
-            q += 1
         else:
             x[q] = 0
-            q += 1
+        q += 1
     
     return x
 
@@ -42,29 +41,26 @@ def biscuit_delivery_gold_value(time):
     for i in time:
         if i >= 6.0:
             x[q] = 150 + mana_gold_value(150)
-            q += 1
         elif i >= 4.0:
             x[q] = 100 + mana_gold_value(100)
-            q += 1
         elif i >= 2.0:
             x[q] = 50 + mana_gold_value(50)
-            q += 1
         else:
             x[q] = 0
-            q += 1
+        q += 1
             
     return x
 
 time = np.arange(0.0,41.0,0.1)
 
-ad_from_gathering_storm = gathering_storm_AD(time)
+gold_value_of_GS = AD_gold_value(gathering_storm_AD(time))
+gold_value_of_BD = biscuit_delivery_gold_value(time)
+gold_value_of_MF = magical_footwear_gold_value(time)
 
-gold_value_of_GS = AD_gold_value(ad_from_gathering_storm)
-
-plt.plot(time,gold_value_of_GS,
-         color="blue",
+plt.plot(time, gold_value_of_GS,
+         color = "blue",
          label = "Gathering Storm")
-plt.plot(time,biscuit_delivery_gold_value(time) + magical_footwear_gold_value(time),
+plt.plot(time, gold_value_of_BD + gold_value_of_MF,
          color = "Green",
          label = "Biscuit Delivery + Magical Footwear")
 plt.xlabel("Time")
